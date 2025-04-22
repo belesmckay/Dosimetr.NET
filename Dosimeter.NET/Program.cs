ff52b5f553429443312f78eb71603167a8136e1b
belesmckay <belesmckay@gmail.com>
2025-04-22 13:57:41 +0200

    Init commit

---
+++ Dosimeter.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO.Ports;
+using MQTTnet;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ahoj");
+    }
+}
